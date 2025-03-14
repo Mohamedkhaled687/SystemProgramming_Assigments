f4683a198df5f5e74edfc8014b6421a000f454f4
MohamedKhaled687 <mohamedkhaled47526@gmail.com>
2025-03-14 12:33:53 +0000

    Adding picoshell code

---
+++ PicoShell_Assigment8/picoshell.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define BUF_SIZE 1024
+#define true (char)1
+
+int main(int argc, char **argv) {
+    char buf[BUF_SIZE];
+    char currentdir[150];
+
+    while (true) {
+        /* Getting the current directory */
+        if (getcwd(currentdir, sizeof(currentdir)) == NULL)
+            printf("Hello in Pico Shell\n");
+        else
+             printf("%s:$ ", currentdir);
+
+        /* Read Input from User */
+        if (!fgets(buf, BUF_SIZE, stdin)) {
+            printf("Error reading input\n");
+            continue;
+        }
+
+        buf[strlen(buf) - 1] = 0; // Remove newline character
+
+        if (strlen(buf) == 0)
+            continue;
+
+        /* Check for the 'exit' command before forking as it isn't external program to execute by execvp*/
+        if (strcmp(buf, "exit") == 0) {
+            printf("Exiting shell...\n");
+            exit(EXIT_SUCCESS);
+        }
+
+        /* Tokenization of the input string */
+        char *args[100];  
+        char *token = strtok(buf, " ");
+        int i = 0;
+
+        while (token != NULL) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        /* Check for the 'cd' command before forking as it isn't also external program to execute*/
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                printf("Missing argument for cd\n");
+            } else {
+                if (chdir(args[1]) != 0) {
+                    perror("cd failed");
+                }
+            }
+            continue;
+        }
+
+        /* Fork and execute external commands */
+        pid_t pid = fork();
+
+        if (pid > 0) {  // Parent process
+            int status;
+            wait(&status);
+
+            if (!WIFEXITED(status)) {
+                printf("Cannot suspend the execution of the current thread\n");
+            }
+        } 
+        else if (pid == 0) {  // Child process
+            execvp(args[0], args);
+            printf("Execution failed\n");
+            exit(EXIT_FAILURE);
+        } 
+        else {  // Fork failed
+            printf("Fork Failed\n");
+        }
+    }
+
+    return 0;
+}
